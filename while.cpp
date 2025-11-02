795b1bb61bcac2edc11ca88c0cf33acbe27de679
taranginidave <taranginidave@gmail.com>
2025-11-02 14:06:26 +0530

    for.cpp

---
+++ while.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 3;
+    int count =1;
+    while(count <= n){
+        cout<< count<<" ";
+        count++;
+    }
+    cout << endl;
+    return 0;
+
+}
