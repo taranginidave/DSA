b18c4de0685c0cc85b4d03a0e4cde03e1d6f0440
taranginidave <taranginidave@gmail.com>
2025-11-03 21:34:51 +0530

    doWhile.cpp

--- for-odd.cpp
+++ for-odd.cpp
@@ -2,11 +2,14 @@
 using namespace std ;
 int main(){
     int n =10;
+    int oddSum =0;
     for(int i=1; i<n; i++){
         if(i%2 !=0){
-            cout<< i<< " ";
+            //cout<< i<< " ";
+            oddSum += i;
         }
     }
+    cout << "odd sum:"<< oddSum<<endl;
     cout <<endl;
     return 0;
 }
