b9c37b985bbb71be8bd8c61822ae5a85be856766
taranginidave <taranginidave@gmail.com>
2025-11-03 21:20:55 +0530

    for-odd.cpp

---
+++ for-odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int n =10;
+    for(int i=1; i<n; i++){
+        if(i%2 !=0){
+            cout<< i<< " ";
+        }
+    }
+    cout <<endl;
+    return 0;
+}
--- for2.cpp
+++ for2.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n =3;
+    int n =50;
     int sum =0;
     for(int i=1; i<=n; i++){
         sum += i;
-    }
+        //if(i == 5){
+            //break;
+        //}
+    //}
     cout<< "sum="<< sum << endl;
     return 0;
 
